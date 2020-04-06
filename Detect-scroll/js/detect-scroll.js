43038710ef69917c705b8cbfa4bdfc91f98e78ab
EricLoBin <ekilobin@gmail.com>
2020-04-06 16:18:01 -0300

    [UPDATE] detect-scroll

--- Detect-scroll/js/detect-scroll.js
+++ Detect-scroll/js/detect-scroll.js
@@ -8,14 +8,37 @@ window.addEventListener('scroll', function () {
 
     var percentage = ((100 * scroll) / totalHeight);
 
-    /**/
+
+
+
+
+    /*Edit this part to create your features*/
+
+
     const h1 = document.getElementsByTagName("h1");
     h1[0].innerHTML = scroll.toFixed() + ' or ' + percentage + '%';
 
 
+    if (percentage == 0){
+        console.log('Top');
+        h1[0].innerHTML = scroll.toFixed() + ' or ' + percentage + '%' + '<br> Top';
+    }
+
+    if (percentage == 100){
+        console.log('Bottom');
+        h1[0].innerHTML = scroll.toFixed() + ' or ' + percentage + '%' + '<br> Bottom';
+    }
+
 
     console.log(scroll.toFixed() + ' ' + (totalHeight));
     console.log('percentage: ' + percentage);
+
+
     /**/
+
+
+
+
+
 
 });
