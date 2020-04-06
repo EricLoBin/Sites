976ce471ef06e4337a7612b08430e4dc3fbc08aa
EricLoBin <ekilobin@gmail.com>
2020-04-06 13:57:22 -0300

    [ADD] detect-scroll

---
+++ Detect-scroll/js/detect-scroll.js
@@ -0,0 +1,12 @@
+const h1 = document.getElementsByTagName('h1');
+
+
+window.addEventListener('scroll', function () {
+    var scroll = window.scrollY;
+    var percentage = document.getElementsByTagName('html');
+
+    /**/
+    console.log(scroll);
+    /**/
+
+});
