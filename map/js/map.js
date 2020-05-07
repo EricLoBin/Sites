ec120a3f68d7fdd452cf28159fc30fb6cc1a97a0
EricLoBin <ekilobin@gmail.com>
2020-05-07 11:34:28 -0300

    [ADD] map

---
+++ map/js/map.js
@@ -0,0 +1,24 @@
+var map = L.map('map').setView([-14.2350, -51.9253], 4);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    minZoom: 1,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery � <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+const locations = [
+    [51.5, -0.09],
+    [11.4, 43],
+];
+
+var indexOfLocations = 0;
+
+while (indexOfLocations < locations.length) {
+    L.marker(locations[indexOfLocations]).addTo(map);
+    indexOfLocations += 1;
+}
